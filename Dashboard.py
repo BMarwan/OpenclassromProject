195fd7369a854d3fea07c73cc42551d70b0bf34c
BMarwan <marwan.boughzala@gmail.com>
2020-06-30 18:22:21 +0200

    Ajout de la visualisation des clients similaires

--- Dashboard.py
+++ Dashboard.py
@@ -1,22 +1,7 @@
-# import streamlit as st
-
-# import numpy as np
-# import pandas as pd
 
 # st.title('Prêt à dépenser')
 # st.write('100001, 100005, 100013, 100028')
 
-# test_corrs_removed = pd.read_csv('test_bureau_corrs_removed.csv')
-
-# user_input = st.text_input("Entrer le numéro du client")
-
-# # st.dataframe(test_corrs_removed[test_corrs_removed['SK_ID_CURR']==user_input])
-
-# #st.write(test_corrs_removed.head())
-
-# if st.button('Entrer'):
-#     st.write(test_corrs_removed[test_corrs_removed['SK_ID_CURR']==user_input])
-#     st.text(user_input)
 
 
 
@@ -29,12 +14,12 @@ from sklearn.model_selection import train_test_split
 from lime.lime_tabular import LimeTabularExplainer
 from sklearn.neighbors import NearestNeighbors
 
+
 import shap
 
 import streamlit as st
 from joblib import dump, load
 import lightgbm as lgb
-
 
 def main():
     st.header("Prêt à dépenser")
@@ -48,39 +33,47 @@ def main():
     
     clf = model_choice()
 
+    
     #Create an input
     user_input = st.text_input("Entrer le numéro du client")
 
-
     if st.button('Rechercher'):
         with st.spinner("Calcul en cours"):
+
+        
+
             st.write(df[df['SK_ID_CURR']==int(user_input)])
+
+
+
             defaut_precentage = clf.predict_proba(test_features.iloc[df.index[df['SK_ID_CURR']==int(user_input)]])[0][1]
             st.write('Probabilité de défaut de paiment : ', np.around(defaut_precentage*100, decimals=2),'%')
-            test = int(user_input)
+            client_id = int(user_input)
             #explainer = shap.TreeExplainer(clf)
             #shap_values = explainer.shap_values(test_features.iloc[0,:])
 
+        with st.spinner("Calcul de l'interprétation"):
+            exp_keys = explain_score(test_features, df, client_id, clf)
 
-            explain_score(test_features, df, test, clf)
+        with st.spinner("Calcul des similarités"):
+            df_similaire = client_similarities(test_features, df, client_id, exp_keys)
+            st.write('Clients similaires :')            
+            st.write(df_similaire)
 
            
             
 
 
 
-#@st.cache
 def load_data():
     df = pd.read_csv('test_bureau_corrs_removed.csv')
     return df
 
-#@st.cache
 def load_models():
     clf_p = load('lgb_pos_weights.joblib')
     clf_n = load('lgb_neg_weights.joblib')
     return clf_p, clf_n
 
-#@st.cache
 def preprocess_data(df):
     test_features = df
     test_features.fillna(test_features.median(), inplace=True)
@@ -88,7 +81,6 @@ def preprocess_data(df):
     test_features = test_features.drop(columns = ['SK_ID_CURR'])
     test_features = pd.get_dummies(test_features)
     return test_features
-
 
 def model_choice():
     #Load Models
@@ -105,7 +97,6 @@ def model_choice():
     return clf
 
 
-#@st.cache
 def explain_score(df_test, df, id, clf):
      lime1 = LimeTabularExplainer(df_test,
                                  feature_names=df_test.columns,
@@ -138,28 +129,37 @@ def explain_score(df_test, df, id, clf):
      plt.gca().invert_yaxis()
 
      for index, value in enumerate(y):
-         plt.text(value, index, str(np.round(value*100, decimals=2)))
+         plt.text(value, index, str(np.round(value, decimals=3)))
      
      plt.title('Explication des résulats')
      plt.xlabel('Poids en pourcentage')   
      plt.tight_layout()
-     #_max_width_()
      st.pyplot()
      #st.write(exp.as_pyplot_figure(), plt.tight_layout())
      #st.write(exp.show_in_notebook(show_all=False))
+     return exp_keys
 
-def _max_width_():
-    max_width_str = f"max-width: 2000px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+def client_similarities(df_test, df, id, exp_keys):
+    
+    #Train the algo
+    neigh = NearestNeighbors(n_neighbors=5, algorithm='auto').fit(df_test)
+    
+    index = df[df["SK_ID_CURR"] == id].index.values
+
+    data_client = df_test.iloc[index[0]]
+    data_client = data_client.values.reshape(1, -1)
+
+    #Find the 5 nearest neighborhoods
+    indices = neigh.kneighbors(data_client, return_distance=False)
+    
+    clients_similaires = pd.DataFrame()
+    #Extract informations of the 5 nearest
+    for i in indices:
+        clients_similaires = clients_similaires.append(df.iloc[i])
+
+    #clients_similaires = clients_similaires[exp_keys]
+    
+    return clients_similaires
 
 
 if __name__ == "__main__":
